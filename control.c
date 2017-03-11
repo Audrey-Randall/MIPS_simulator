ec766243bad8c0227c2fb8670ff602c02aa5fbac
Audrey Randall <audrey.randall@colorado.edu>
2017-03-11 10:44:41 -0700

    Added control logic

--- control.c
+++ control.c
@@ -3,8 +3,48 @@
 //Figure 4.22 tells how to implement this
 int setControls(uint8_t opcode){
   switch(opcode){
-    case ???:
+    case OPCODE_R:
+      controlUnit.RegDst = 1;
+      controlUnit.ALUsrc = 0;
+      controlUnit.MemToReg = 0;
+      controlUnit.RegWrite = 1;
+      controlUnit.MemRead = 0;
+      controlUnit.MemWrite = 0;
+      controlUnit.Branch = 0;
+      controlUnit.ALUop = 0b10;
       break;
-    default: return -1;
+    case OPCODE_LW:
+      controlUnit.RegDst = 0;
+      controlUnit.ALUsrc = 1;
+      controlUnit.MemToReg = 1;
+      controlUnit.RegWrite = 1;
+      controlUnit.MemRead = 1;
+      controlUnit.MemWrite = 0;
+      controlUnit.Branch = 0;
+      controlUnit.ALUop = 0b00;
+      break;
+    case OPCODE_SW:
+      controlUnit.RegDst = 0;  //X in diagram in book
+      controlUnit.ALUsrc = 1;
+      controlUnit.MemToReg = 0;  //X in book
+      controlUnit.RegWrite = 0;
+      controlUnit.MemRead = 0;
+      controlUnit.MemWrite = 1;
+      controlUnit.Branch = 0;
+      controlUnit.ALUop = 0b00;
+      break;
+    case OPCODE_BEQ:
+      controlUnit.RegDst = 0;  //X in book
+      controlUnit.ALUsrc = 0;
+      controlUnit.MemToReg = 0;  //X in book
+      controlUnit.RegWrite = 0;
+      controlUnit.MemRead = 0;
+      controlUnit.MemWrite = 0;
+      controlUnit.Branch = 1;
+      controlUnit.ALUop = 0b01;
+      break;
+    default:
+      return -1;
+    return 0;
   }
 }
--- control.h
+++ control.h
@@ -1,8 +1,13 @@
 #ifndef CONTROL
 #define CONTROL
 
+#define OPCODE_R 0b000000
+#define OPCODE_LW 0b100011
+#define OPCODE_SW 0b101011
+#define OPCODE_BEQ 0b000100
+
 typedef struct ControlUnit {
-  uint8_t ALUop:4;
+  uint8_t ALUop:2;
   uint8_t ALUsrc:1;
   uint8_t RegWrite:1;
   uint8_t MemWrite:1;
@@ -10,9 +15,10 @@ typedef struct ControlUnit {
   uint8_t MemRead:1;
   uint8_t PCsrc:1;
   uint8_t RegDst:1;
+  uint8_t Branch:1;
 }ControlUnit;
 
 ControlUnit controlUnit;
-int setControls(); 
+int setControls();
 
 #endif
