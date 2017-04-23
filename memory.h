d05342375f57c09022c11de55d82063ac727b171
Audrey Randall <audrey.randall@colorado.edu>
2017-04-23 10:51:26 -0600

    Writeback and memory stages 'complete' for a given value of complete, time to test this thing

---
+++ memory.h
@@ -0,0 +1,21 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#define MEMSIZE 4096
+
+//Three block sizes: 1, 4, 16
+#define BLOCKSIZE 16
+
+//Use writeback or write-through
+//On write-through, any instruction that writes to the cache writes to memory at the same time
+#define WRITEBACK 0
+
+//Cache sizes
+#define ICACHESIZE 128
+#define DCACHESIZE 256
+
+//TODO: declare write buffer. Block sized?
+
+uint32_t memory[MEMSIZE];    //Program memory
+int32_t dataMem[MEMSIZE];    //Data memory. TODO: Should this be signed or unsigned or does it matter?
+#endif
